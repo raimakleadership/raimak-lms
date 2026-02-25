43d61fee7f32518c2e4cb9ef2f3590ba4575a09c
Richard Zacker <richardzacker@gmail.com>
2026-02-25 15:01:35 -0600

    Fix config.js by adding quotes to IDs

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 const Config = {
   azure: {
-    clientId: ad1b153f-8b6a-4f1c-8ab2-58fcf03cf5c2,
-    tenantId: 39e14190-0b23-4ecd-99f9-606ad1215881,
+    clientId: "ad1b153f-8b6a-4f1c-8ab2-58fcf03cf5c2",
+    tenantId: "39e14190-0b23-4ecd-99f9-606ad1215881",
     redirectUri: window.location.origin + window.location.pathname
   },
   sharePoint: {
