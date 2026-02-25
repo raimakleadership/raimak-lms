7d4f5faca698a5e8f12f607ef8340efbe67e8fb0
Richard Zacker <richardzacker@gmail.com>
2026-02-25 14:52:43 -0600

    Update configuration values in config.js

--- js/config.js
+++ js/config.js
@@ -1,29 +1,29 @@
 const Config = {
   azure: {
-    clientId:    ad1b153f-8b6a-4f1c-8ab2-58fcf03cf5c2,
-    tenantId:    39e14190-0b23-4ecd-99f9-606ad1215881,
-    redirectUri: window.location.origin + window.location.pathname,
+    clientId: "PASTE_YOUR_CLIENT_ID_HERE",
+    tenantId: "PASTE_YOUR_TENANT_ID_HERE",
+    redirectUri: window.location.origin + window.location.pathname
   },
   sharePoint: {
     hostname: "raimak.sharepoint.com",
     sites: {
       leadship: "sites/RaimakLeadship",
-      team:     "TeamSite",
+      team: "TeamSite"
     },
     lists: {
-      activityLog:    "2adb1260-e635-45cd-bb3b-87dd57a2d022",
+      activityLog: "2adb1260-e635-45cd-bb3b-87dd57a2d022",
       contractorList: "bd5df38a-9cb6-411d-87e8-3e79934213d3",
-      leadsList:      "5a01419d-e2c9-4aad-8484-6ed97233f305",
+      leadsList: "5a01419d-e2c9-4aad-8484-6ed97233f305"
     },
-    graphBase: "https://graph.microsoft.com/v1.0",
+    graphBase: "https://graph.microsoft.com/v1.0"
   },
   rules: {
-    coolOffDays:      2,
+    coolOffDays: 2,
     maxLeadsPerAgent: 15,
     recycleAfterDays: 30,
-    appVersion:       "1.0",
+    appVersion: "1.0"
   },
   leadStatuses: ["New","Contacted","Qualified","Proposal Sent","Negotiating","Won","Lost","Recycled"],
-  leadSources:  ["Web Form","Referral","Cold Call","Email Campaign","Social Media","Trade Show","Other"],
-  scopes: ["Sites.ReadWrite.All","User.Read"],
+  leadSources: ["Web Form","Referral","Cold Call","Email Campaign","Social Media","Trade Show","Other"],
+  scopes: ["Sites.ReadWrite.All","User.Read"]
 };
